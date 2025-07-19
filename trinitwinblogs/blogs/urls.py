7d960b2e4a992abed7a91a38d4ff5b7cf4ebd71d
Tani T <tani.abereoje@gmail.com>
2025-07-19 19:19:25 +0100

    Set up views for displaying database contend on the frontend, Set up urls and renamed template/trinitwinblogs to blogs

--- trinitwinblogs/blogs/urls.py
+++ trinitwinblogs/blogs/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    # path('', )
+    path('home/', views.summarizedPost, name='home')
 ]
--- trinitwinblogs/blogs/views.py
+++ trinitwinblogs/blogs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import BlogPost
 
 # Create your views here.
+
+def summarizedPost(request):
+    posts = BlogPost.objects.select_related('blogger').values('title', 'summary', 'updated', 'blogger__author_name')
+    context = {
+        'posts':posts
+    }
+    return render(request, 'blogs/home.html' ,context)
